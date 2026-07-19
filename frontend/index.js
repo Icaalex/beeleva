e9685a3fb8f294f0f51ec061e9a9984a2201aa32
Icaalex <ndukwuuju6@gmail.com>
2026-07-19 13:49:13 -0400

    update frontend to use live production URL

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-  const API = 'http://127.0.0.1:8000';
+  const API = 'https://beeleva-production.up.railway.app/';
 
   // Show toast notification
   function showToast(message, type = 'success') {
